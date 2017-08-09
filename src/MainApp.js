1e61cb19ff5a64823d1f9e29068c4bcfca87c8b0
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2017-08-09 15:07:25 +0700

    chap1: Creating a toggle button

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,67 +1,65 @@
-import React from 'react'
-import { StyleSheet, Image, View } from 'react-native'
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  TouchableHighlight
+} from 'react-native'
 
-const playIcon = require('./images/play.png')
-const volumeIcon = require('./images/sound.png')
-const hdIcon = require('./images/hd-sign.png')
-const fullScreenIcon = require('./images/full-screen.png')
-const remoteImage = { uri: 'https://s3.amazonaws.com/crysfel/public/book/new-york.jpg' }
+const heartIcon = require('./images/plain-heart.png')
 
-const MainApp = () => {
-  return (
-    <Image source={remoteImage} style={styles.fullscreen}>
+class MainApp extends Component {
+  state = {
+    liked: false
+  }
+
+  _onPressBtn = () => {
+    this.setState(prevState => ({
+      liked: !prevState.liked
+    }))
+  }
+
+  render () {
+    const likedStyles = this.state.liked ? styles.liked : null
+
+    return (
       <View style={styles.container}>
-        <Image source={playIcon} style={styles.icon} />
-        <Image source={volumeIcon} style={styles.icon} />
-        <View style={styles.progress}>
-          <View style={styles.progressBar} />
-        </View>
-        <Image source={hdIcon} style={styles.icon} />
-        <Image source={fullScreenIcon} style={styles.icon} />
+        <TouchableHighlight
+          onPress={this._onPressBtn}
+          style={styles.btn}
+          underlayColor='#fefefe'
+        >
+          <Image
+            source={heartIcon}
+            style={[styles.icon, likedStyles]}
+          />
+        </TouchableHighlight>
+        <Text style={styles.text}>Do u like this app?</Text>
       </View>
-    </Image>
-  )
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  fullscreen: {
-    flex: 1
-  },
   container: {
-    position: 'absolute',
-    backgroundColor: '#202020',
+    marginTop: 50,
+    alignItems: 'center'
+  },
+  btn: {
     borderRadius: 5,
-    flexDirection: 'row',
-    height: 50,
-    padding: 5,
-    paddingTop: 16,
-    bottom: 30,
-    right: 10,
-    left: 10,
-    borderWidth: 1,
-    borderColor: '#303030'
+    padding: 10
   },
   icon: {
-    tintColor: '#fff',
-    height: 16,
-    width: 16,
-    marginLeft: 5,
-    marginRight: 5
+    width: 180,
+    height: 180,
+    tintColor: '#f1f1f1'
   },
-  progress: {
-    backgroundColor: '#000',
-    borderRadius: 7,
-    flex: 1,
-    height: 14,
-    margin: 10,
-    marginTop: 2
+  liked: {
+    tintColor: '#e74c3c'
   },
-  progressBar: {
-    backgroundColor: '#bf161c',
-    borderRadius: 5,
-    height: 10,
-    margin: 2,
-    width: 80
+  text: {
+    marginTop: 20
   }
 })
 
