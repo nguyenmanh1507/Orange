27d4dc80dc38474df9b214a52f7149b1fb2e63a5
--global <nguyenmanh1507@gmail.com>
2017-08-10 15:33:55 +0700

    :art: chap2: Dealing with universal apps

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,38 +1,35 @@
-import React from 'react'
-import EvilIcon from 'react-native-vector-icons/EvilIcons'
-import FAIcon from 'react-native-vector-icons/FontAwesome'
-import { StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+import Dimensions from './utils/Dimensions'
+import data from './data.json'
+import UserList from './UserList'
+import UserDetail from './UserDetail'
 
-const MainApp = () => (
-  <View style={styles.content}>
-    <View style={styles.row}>
-      <EvilIcon style={styles.icon} name='user' size={50} color='#e67e22' />
-      <EvilIcon style={styles.icon} name='search' size={50} color='#e67e22' />
-      <EvilIcon style={styles.icon} name='trophy' size={50} color='#e67e22' />
-      <EvilIcon style={styles.icon} name='location' size={50} color='#e67e22' />
-      <EvilIcon style={styles.icon} name='gear' size={50} color='#e67e22' />
-    </View>
-    <View style={styles.row}>
-      <FAIcon style={styles.icon} name='briefcase' size={40} color='#c0392b' />
-      <FAIcon style={styles.icon} name='calendar' size={40} color='#c0392b' />
-      <FAIcon style={styles.icon} name='camera-retro' size={40} color='#c0392b' />
-      <FAIcon style={styles.icon} name='globe' size={40} color='#c0392b' />
-      <FAIcon style={styles.icon} name='print' size={40} color='#c0392b' />
-    </View>
-  </View>
-)
+class MainApp extends Component {
+  renderMaster () {
+    return <UserList contacts={data.results} />
+  }
+
+  renderDetail () {
+    if (Dimensions.isTablet()) {
+      return <UserDetail contact={data.results[0]} />
+    }
+  }
+
+  render () {
+    return (
+      <View style={styles.content}>
+        {this.renderMaster()}
+        {this.renderDetail()}
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   content: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  row: {
     flexDirection: 'row'
-  },
-  icon: {
-    margin: 10
   }
 })
 
