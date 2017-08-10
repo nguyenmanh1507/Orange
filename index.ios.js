7356d9c9bfbe106ecb7b45daa65764794cfba798
--global <nguyenmanh1507@gmail.com>
2017-08-10 13:46:44 +0700

    :art: chap2: Including custom fonts on iOS

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,44 @@
-import { AppRegistry } from 'react-native'
-import MainApp from './src/MainApp'
+// import { AppRegistry } from 'react-native'
+// import MainApp from './src/MainApp'
+//
+// AppRegistry.registerComponent('Orange', () => MainApp)
 
-AppRegistry.registerComponent('Orange', () => MainApp)
+import React from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+
+const CustomFontsIos = () => (
+  <View style={styles.container}>
+    <Text style={styles.base}>Welcome to React Native!</Text>
+    <Text style={[styles.base, styles.italic]}>Welcome to React Native!</Text>
+    <Text style={[styles.base, styles.light]}>Welcome to React Native!</Text>
+    <Text style={[styles.base, styles.bold]}>Welcome to React Native!</Text>
+    <Text style={[styles.base, styles.dancing]}>Welcome to React Native!</Text>
+    <Text style={[styles.base, styles.indie]}>Welcome to React Native!</Text>
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e74c3c'
+  },
+  base: {
+    color: '#fff',
+    fontFamily: 'Vollkorn',
+    fontSize: 25,
+    textAlign: 'center',
+    margin: 5,
+    fontWeight: '700'
+  }
+})
+
+AppRegistry.registerComponent(
+  'Orange', () => CustomFontsIos
+)
