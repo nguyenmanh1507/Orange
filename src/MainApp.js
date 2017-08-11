d03037eb32512dfd527b7f6d54ed087992f76cd6
--global <nguyenmanh1507@gmail.com>
2017-08-11 14:52:44 +0700

    :art: chap2: How to create a form component

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,65 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {
-  Alert,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text
-} from 'react-native'
-import HTMLView from 'react-native-htmlview'
-import data from './data.json'
+   Alert,
+   StyleSheet,
+   ScrollView,
+   View,
+   Text,
+   TextInput
+ } from 'react-native'
+import UserForm from './UserForm'
 
-class MainApp extends Component {
-  onLinkPress (url) {
-    Alert.alert('Link press', `URL: ${url}`)
-  }
-
-  render () {
-    return (
-      <View style={styles.main}>
-        <Text style={styles.title}>{data.title}</Text>
-        <ScrollView style={styles.content}>
-          <HTMLView
-            value={data.content}
-            stylesheet={styles}
-            onLinkPress={this.onLinkPress}
-            addLineBreaks={false}
-          />
-        </ScrollView>
-      </View>
-    )
-  }
-}
+const MainApp = () => (
+  <View style={styles.main}>
+    <Text style={styles.toolbar}>Fitness App</Text>
+    <ScrollView style={styles.content}>
+      <UserForm />
+    </ScrollView>
+  </View>
+)
 
 const styles = StyleSheet.create({
   main: {
-    flex: 1
+    flex: 1,
+    backgroundColor: '#ecf0f1'
+  },
+  toolbar: {
+    backgroundColor: '#1abc9c',
+    padding: 20,
+    color: '#fff',
+    fontSize: 20
   },
   content: {
-    flex: 1,
     padding: 10
-  },
-  title: {
-    backgroundColor: '#c0392b',
-    color: '#fff',
-    padding: 20,
-    fontSize: 20,
-    textAlign: 'center'
-  },
-  p: {
-    color: '#333',
-    fontSize: 16,
-    marginBottom: 16
-  },
-  h3: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#000',
-    marginBottom: 16
-  },
-  a: {
-    color: '#c0392b',
-    fontWeight: 'normal'
   }
 })
 
