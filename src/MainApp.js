6a8b043357a7750a05764baaa2d014673be5eb8d
--global <nguyenmanh1507@gmail.com>
2017-08-14 10:13:17 +0700

    :art: chap3: Running several animations at the same time

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,55 +1,94 @@
 import React, { Component } from 'react'
 import {
+  View,
   Animated,
+  Image,
   Easing,
   Dimensions,
   StyleSheet
 } from 'react-native'
 
-const { width, height } = Dimensions.get('window')
-const cloudImage = require('./images/cloudy.png')
-const imageWidth = 80
+const { width, height } = Dimensions.get('window');
+const cloudImage = require('./images/cloud.png');
+const cloudsImage = require('./images/cloudy.png');
+const planeImage = require('./images/transport.png');
+const cloudWidth = 60;
 
 class MainApp extends Component {
-  startAnimation () {
-    this.animatedValue.setValue(width)
+  componentWillMount() {
+    this.animatedValue = new Animated.Value()
+  }
+
+  componentDidMount() {
+    this.startAnimation()
+  }
+
+  startAnimation() {
+    this.animatedValue.setValue(1)
     Animated.timing(
       this.animatedValue,
       {
-        toValue: -imageWidth,
+        toValue: 0,
         duration: 6000,
         easing: Easing.linear
       }
     ).start(() => this.startAnimation())
   }
 
-  componentWillMount () {
-    this.animatedValue = new Animated.Value()
-  }
+  render() {
+    const left1 = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-cloudWidth, width]
+    })
 
-  componentDidMount () {
-    this.startAnimation()
-  }
+    const left2 = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-cloudWidth * 5, width + cloudWidth * 5]
+    })
 
-  render () {
     return (
-      <Animated.Image
-        style={[
-          styles.image,
-          { left: this.animatedValue }
-        ]}
-        source={cloudImage}
-      />
+      <View>
+        <Animated.Image
+          style={[
+            styles.cloud1,
+            { left: left1 }
+          ]}
+          source={cloudImage}
+        />
+        <Animated.Image
+          style={[
+            styles.cloud1,
+            styles.cloud2,
+            { left: left2 }
+          ]}
+          source={cloudImage}
+        />
+        <Image
+          style={[styles.cloud1, styles.plane]}
+          source={planeImage}
+        />
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  image: {
-    height: imageWidth,
+  cloud1: {
     position: 'absolute',
-    top: height / 3,
-    width: imageWidth
+    width: cloudWidth,
+    height: cloudWidth,
+    top: height / 3 - cloudWidth / 2
+  },
+  cloud2: {
+    width: cloudWidth * 1.5,
+    height: cloudWidth * 1.5,
+    top: height / 2
+  },
+  plane: {
+    width: cloudWidth * 1.3,
+    height: cloudWidth * 1.3,
+    top: height / 2 - cloudWidth,
+    left: width / 2 - cloudWidth / 2
   }
 })
 
